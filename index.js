a07bf1cd670722921e31a9c0f6bf9d7bca7777eb
Fith Wahome <wahomeimani@gmail.com>
2023-10-01 17:15:41 +0300

    assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+const mainElement = document.getElementById('main');
+mainElement.remove();
+
+// Create a new h1 header element
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory'; // Set the ID attribute
+newHeader.textContent = 'YOUR-NAME is the champion'; // Set the text content
+document.body.appendChild(newHeader)
+
+
+
+
